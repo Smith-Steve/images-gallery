23852d1ea138819bf462ca269e0220a42f24d7ba
smith-steve <Steve.A.Dore@outlook.com>
2024-02-18 22:20:25 -0800

    bedtime commit.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
 
-const Header = () => {
+const Header = (props) => {
     return (
         <Navbar bg="light" data-bs-theme="light">
           <Navbar.Brand href="/">Images Gallery</Navbar.Brand>
