62a05564af5d173b47c555c1e048710fd1b861fc
smith-steve <Steve.A.Dore@outlook.com>
2024-02-19 12:29:02 -0800

    frond end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import Header from'./components/Header'
+import Search from './components/Search'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Component } from 'react';
 
 const App = () =>  {
   return (
     <div>
       <Header title="Images Gallery"/>
+      <Search/>
     </div>
   );
 }
