d6d75df143292cbd0bf52e67c1ff32567fb1ba5b
smith-steve <Steve.A.Dore@outlook.com>
2024-02-19 12:58:48 -0800

    small front end changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
+
+const navBarStyle = {
+    backgroundColor:'lightblue' 
+};
 
 const Header = ({title}) => {
     return (
-        <Navbar bg="light" data-bs-theme="light">
+        <Navbar style={navBarStyle} data-bs-theme="light">
+            <Container>
           <Navbar.Brand href="/">{title}</Navbar.Brand>
+            </Container>
       </Navbar>
     )
 }
