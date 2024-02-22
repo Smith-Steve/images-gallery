fa7b5abf0b4cd2a26aba461abb248d463a644dcc
smith-steve <Steve.A.Dore@outlook.com>
2024-02-22 10:35:43 -0800

    added catch

--- api/main.py
+++ api/main.py
@@ -8,6 +8,10 @@ load_dotenv(dotenv_path="./.env.local")
 UNSPLASH_URL='https://api.unsplash.com/photos/random'
 UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
 
+if not UNSPLASH_KEY:
+  #in python empty string is falsey.
+  raise EnvironmentError("Please create .env.local file and insert unsplash API key.")
+
 
 application = Flask(__name__)
 #in python, constants are uppercase.
