73b0df0e8cd800516512d0824c0930ecf61b222a
smith-steve <Steve.A.Dore@outlook.com>
2024-02-21 20:00:47 -0800

    small nothing change again.

--- api/app.py
+++ api/app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask;
 
-app = Flask(__name__)
 
-@app.route("/")
+application = Flask(__name__)
+
+
+@application.route("/")
 def hello():
   return "Hello World"
+
+if __name__ == "__main__":
+  application.run(host="0.0.0.0", port=5050)
