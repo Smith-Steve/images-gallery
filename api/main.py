6161431b62f574ee4441c7ad4705a7576f84c574
smith-steve <Steve.A.Dore@outlook.com>
2024-02-22 21:40:07 -0800

    added docker file.

--- api/main.py
+++ api/main.py
@@ -7,9 +7,9 @@ from flask_cors import CORS
 
 load_dotenv(dotenv_path="./.env.local")
 
-UNSPLASH_URL='https://api.unsplash.com/photos/random'
-UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
-DEBUG=bool(os.environ.get("DEBUG", True))
+UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
   #in python empty string is falsey.
